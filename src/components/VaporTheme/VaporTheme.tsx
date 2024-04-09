e5970ca79c21ec09a1d20e2ac1d868e8c2cc5f72
Tomaster134 <tomaster134@gmail.com>
2024-04-09 09:51:17 -0400

    initial commit

---
+++ src/components/VaporTheme/VaporTheme.tsx
@@ -0,0 +1,45 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#ff61c6',
+    },
+    secondary: {
+      main: '#5cecff',
+    },
+    background: {
+      default: '#0a0c37',
+      paper: 'rgba(55,89,113,0.25)',
+    },
+    warning: {
+      main: '#ff9900',
+    },
+    info: {
+      main: '#5cecff',
+    },
+    divider: 'rgba(244,255,97,0.5)',
+  },
+  typography: {
+    fontFamily: 'Arimo',
+    h5: {
+      fontFamily: 'Roboto Mono',
+    },
+    h4: {
+      fontFamily: 'Roboto Mono',
+    },
+    h3: {
+      fontFamily: 'Roboto Mono',
+    },
+    h2: {
+      fontFamily: 'Roboto Mono',
+    },
+    h1: {
+      fontFamily: 'Roboto Mono',
+    },
+    h6: {
+      fontFamily: 'Roboto Mono',
+    },
+  },
+};
